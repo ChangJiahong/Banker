c63c524af32494abbdc29fff5f40708387cdb9f5
ChangJiahong <2327085154@qq.com>
2025-07-24 10:13:38 +0800

    增加menu菜单

--- composeApp/src/commonMain/kotlin/cn/changjiahong/banker/app/template/TemplateSettingScreen.kt
+++ composeApp/src/commonMain/kotlin/cn/changjiahong/banker/app/template/TemplateSettingScreen.kt
@@ -1,11 +1,18 @@
 package cn.changjiahong.banker.app.template
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -15,7 +22,17 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.isSecondaryPressed
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.DpOffset
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import banker.composeapp.generated.resources.Res
 import banker.composeapp.generated.resources.add_box
@@ -74,8 +91,23 @@ fun TempFileGridView(
         horizontalArrangement = Arrangement.Center, verticalArrangement = Arrangement.Center
     ) {
         itemsIndexed(tempFiles) { index, item ->
-            FoldersButton(item.templateName, painterResource(Res.drawable.pdf)) {
-                TempSettingUiEvent.GoPreTemplateScreen(item).sendTo(tempSettingScreenModel)
+            RightClickMenu(menu = {
+                DropdownMenuItem(text = {
+                    Text("预览")
+                }, onClick = {
+                    TempSettingUiEvent.GoPreTemplateScreen(item).sendTo(tempSettingScreenModel)
+                    it()
+                })
+                DropdownMenuItem(text = {
+                    Text("查看表单")
+                }, onClick = {
+                    println("菜单项2点击")
+                    it()
+                })
+            }) {
+                FoldersButton(item.templateName, painterResource(Res.drawable.pdf)) {
+                    TempSettingUiEvent.GoPreTemplateScreen(item).sendTo(tempSettingScreenModel)
+                }
             }
         }
 
@@ -85,4 +117,49 @@ fun TempFileGridView(
             }
         }
     }
+}
+
+
+@Composable
+fun RightClickMenu(
+    menu: @Composable ColumnScope.(close: () -> Unit) -> Unit,
+    content: @Composable BoxScope.() -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+    var offset by remember { mutableStateOf(DpOffset(0.dp, 0.dp)) }
+    Box(
+        modifier = Modifier
+            .wrapContentSize()
+            .pointerInput(Unit) {
+                awaitPointerEventScope {
+                    while (true) {
+                        val event = awaitPointerEvent()
+                        val mouseEvent = event.buttons
+
+                        if (mouseEvent.isSecondaryPressed) {
+                            val position = event.changes.first().position
+                            offset = DpOffset(position.x.toDp(), position.y.toDp())
+                            expanded = true
+                        }
+                    }
+                }
+            }) {
+
+
+        Box {
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                offset = offset
+            ) {
+                menu {
+                    expanded = false
+                }
+            }
+
+        }
+
+        content()
+    }
+
 }
